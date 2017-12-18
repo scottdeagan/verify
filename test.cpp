bede4c22673a7a94553cd529b2a7f3e59a5f16cb
scottdeagan <scott.deagan@gmail.com>
2017-12-18 23:23:36 +0000

    This is another change to test pull-requests from the terminal. Let's hope that a pull request will appear on the Github web UI.

--- test.cpp
+++ test.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[])
-{
+{ 
+  printf("This is another change that should appear as a pull request...");
   printf("This another line that will be added...\n");
   printf("Hello there...\n");
   return 0;
